bcc333b22b8de4d8a9e5baba17863ab79ab3c865
UserName <User@Email.com>
2025-02-07 02:01:29 +0000

    bugfix validation

--- variables.tf
+++ variables.tf
@@ -57,29 +57,6 @@ variable "oidc_role_name" {
 variable "oidc_connector" {
   type        = string
   description = "Which OIDC connector to use, one of [BitBucket, GitHub]"
-  validation {
-    condition = anytrue([
-      alltrue([
-        (var.oidc_connector == "BitBucket"),
-        (var.oidc_bitbucket_repository_uuid != null),
-        (var.oidc_bitbucket_thumbprint != null),
-        (var.oidc_bitbucket_workspace_name != null),
-        (var.oidc_bitbucket_workspace_uuid != null),
-        (var.oidc_github_account_name == null),
-        (var.oidc_github_repository_name == null),
-      ]),
-      alltrue([
-        (var.oidc_connector == "GitHub"),
-        (var.oidc_bitbucket_repository_uuid == null),
-        (var.oidc_bitbucket_thumbprint == null),
-        (var.oidc_bitbucket_workspace_name == null),
-        (var.oidc_bitbucket_workspace_uuid == null),
-        (var.oidc_github_account_name != null),
-        (var.oidc_github_repository_name != null),
-      ]),
-    ])
-    error_message = "oidc_connector must be one of [BitBucket, GitHub]. All it's associated oidc_... variables must not be null. All other unused oidc_... variables must be null."
-  }
 }
 
 variable "oidc_bitbucket_workspace_name" {
@@ -122,4 +99,32 @@ variable "oidc_github_repository_name" {
   default     = null
   type        = string
   description = "GitHub specific OIDC connection | Repository name"
+}
+
+resource "null_resource" "validate_inputs" {
+  lifecycle {
+    precondition {
+      condition = anytrue([
+        alltrue([
+          (var.oidc_connector == "BitBucket"),
+          (var.oidc_bitbucket_repository_uuid != null),
+          (var.oidc_bitbucket_thumbprint != null),
+          (var.oidc_bitbucket_workspace_name != null),
+          (var.oidc_bitbucket_workspace_uuid != null),
+          (var.oidc_github_account_name == null),
+          (var.oidc_github_repository_name == null),
+        ]),
+        alltrue([
+          (var.oidc_connector == "GitHub"),
+          (var.oidc_bitbucket_repository_uuid == null),
+          (var.oidc_bitbucket_thumbprint == null),
+          (var.oidc_bitbucket_workspace_name == null),
+          (var.oidc_bitbucket_workspace_uuid == null),
+          (var.oidc_github_account_name != null),
+          (var.oidc_github_repository_name != null),
+        ]),
+      ])
+      error_message = "oidc_connector must be one of [BitBucket, GitHub]. All it's associated oidc_... variables must not be null. All other unused oidc_... variables must be null."
+    }
+  }
 }
