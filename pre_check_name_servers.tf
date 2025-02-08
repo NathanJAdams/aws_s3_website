32caf9bcaad966ec2bc7cf6977294628d0224dd6
UserName <User@Email.com>
2025-02-08 16:26:42 +0000

    test check

--- pre_check_name_servers.tf
+++ pre_check_name_servers.tf
@@ -14,10 +14,20 @@ locals {
 }
 
 resource "null_resource" "pre_check_name_servers" {
+  count = 0
+
   lifecycle {
     precondition {
       condition     = local.actual_name_servers == local.expected_name_servers
       error_message = "The name servers retrieved from https://dns.google/resolve?type=NS&name=[my-domain.com] must match those set on the hosted zone."
     }
   }
+}
+
+output "actual_name_servers" {
+  value = local.actual_name_servers
+}
+
+output "expected_name_servers" {
+  value = local.expected_name_servers
 }
