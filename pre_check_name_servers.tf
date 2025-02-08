c14b660dbd8d606cbe0ebbacd1c9382f9c8189bf
UserName <User@Email.com>
2025-02-08 16:22:14 +0000

    test check

--- pre_check_name_servers.tf
+++ pre_check_name_servers.tf
@@ -1,5 +1,5 @@
 data "http" "dns_google_name_servers" {
-  url = "https://dns.google/resolve?name=${var.bare_domain}&type=NS"
+  url = "https://dns.google/resolve?type=NS&name=${var.bare_domain}"
 
   request_headers = {
     Accept = "application/dns-json"
@@ -7,17 +7,17 @@ data "http" "dns_google_name_servers" {
 }
 
 locals {
+  bare_domain = var.bare_domain
   actual_name_servers_response = jsondecode(data.http.dns_google_name_servers.response_body)
   actual_name_servers = sort([for answer in lookup(local.actual_name_servers_response, "Answer", []) : answer.data])
   expected_name_servers = sort(data.aws_route53_zone.zone.name_servers)
-  zone_name = data.aws_route53_zone.zone.name
 }
 
 resource "null_resource" "pre_check_name_servers" {
   lifecycle {
     precondition {
       condition     = local.actual_name_servers == local.expected_name_servers
-      error_message = "The name servers for ${var.bare_domain}: ${local.actual_name_servers} must match those from it's hosted zone ${local.zone_name}: ${local.expected_name_servers}."
+      error_message = "The name servers retrieved from https://dns.google/resolve?type=NS&name=[my-domain.com] must match those set on the hosted zone."
     }
   }
 }
